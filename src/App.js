a8ef37abfbd2dda6fb6bcee35c4af3fbcb74653f
pangsitmie <pangsitmie@github.com>
2022-06-24 14:44:53 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import fleekLogo from './fleek-logo.png';
+import reactLogo from './react-logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={fleekLogo} className="Fleek-logo" alt="fleek-logo" />
+        <span className="Big-plus">+</span>
+        <img src={reactLogo} className="React-logo" alt="react-logo" />
         <p>
-          Hello <code>src/App.js</code> and save to reload.
+          React App deployed on IPFS through Fleek.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docs.fleek.co"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More About Fleek!
         </a>
       </header>
     </div>
